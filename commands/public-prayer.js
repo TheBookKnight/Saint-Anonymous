394f03b9b84739b8235ac548166b7bc9fd817c36
Joshua Cadavez <>
2022-03-25 00:17:29 -0400

    Add prayer emojis

--- commands/public-prayer.js
+++ commands/public-prayer.js
@@ -9,7 +9,7 @@ module.exports = {
             .setDescription('What would you like to have prayers for?')
             .setRequired(true)),
         async execute(interaction) {
-            const prayer = `Prayer Request:\n${interaction.options.getString('prayer')}\n\nRequester:\n${interaction.user.tag}`;
+            const prayer = `Prayer Request 🙏\n${interaction.options.getString('prayer')}\n\nRequester:\n${interaction.user.tag}`;
             await interaction.reply(prayer);
         }
 }
