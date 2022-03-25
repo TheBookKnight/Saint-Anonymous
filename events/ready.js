5f98d0e18500fc0c734704a8bbc89c2378fbf5cf
Joshua Cadavez <>
2022-03-25 00:47:40 -0400

    Add tutorial command

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
+		console.log('Saint Anonymous is ready to share your prayers!');
 	},
 };
