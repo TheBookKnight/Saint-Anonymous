31fede5baf6cfcc7797cf1d5107bfc72b94a673d
Joshua Cadavez <>
2022-04-02 00:38:52 -0400

    Fix disconnect bug

--- commands/stop-music.js
+++ commands/stop-music.js
@@ -5,9 +5,10 @@ module.exports = {
         .setName('stop-music')
         .setDescription('Stop music.'),
         async execute(message) {
-            if(message.guildId) {
-                const voice = require('@discordjs/voice');
-                voice.getVoiceConnection(message.guildId).disconnect();
+            const voice = require('@discordjs/voice');
+            const connection = voice.getVoiceConnection(message.guildId);
+            if(connection) {
+                connection.disconnect();
                 message.reply({content: "Saint Anonymous stopped playing music.", ephemeral: true});
             } else {
                 message.reply({content: "Saint Anonymous already stopped playing music.", ephemeral: true});
