e2ef4526dce2d20250469b480ba409f110b7a2a0
Joshua Cadavez <>
2022-04-11 16:06:02 -0400

    Update Rosary cmd

--- commands/rosary.js
+++ commands/rosary.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const message = 'Hello, I am Saint Anonymous.\n\nMy calling is to share your prayers anonymously. I am ordained by Joshua Cadavez, the almighty but humble tech master.\n\nRevere him with 10 decades of the Rosary.';
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -17,23 +17,35 @@ module.exports = {
             if (typeof rosaryChannel === 'undefined') {
                 return await interaction.reply({content: "There's no #rosary Voice channel. Please create one before trying again.", ephemeral: true});
             }
+
             let members = rosaryChannel.members;
+            if(members.length === 0) {
+                return await interaction.reply({content: "There's no one on the #rosary Voice channel.", ephemeral: true});
+            }
+
             members = Array.from(members, ([id, info]) => ({id}));
             
             // randomizes choice
             let choose = Math.floor(Math.random() * members.length);
 
+            // prepares who reads the decades and mysteries
             let decades = ['1st Decade:\t', '2nd Decade:\t', '3rd Decade:\t', '4th Decade:\t', '5th Decade:\t']
             for(let decade=0; decade<decades.length; decade++) {
                 decades[decade] = decades[decade] + `<@!${members[choose].id}>`;
                 choose = (choose + 1) % members.length;
             }
             choose = (choose + 1) % members.length;
-            decades.unshift(
-                'VIRTUAL ROSARY',
-                `Read Mysteries:\t<@!${members[choose].id}>`)
+            decades.unshift(`Read Mysteries:\t<@!${members[choose].id}>`)
             decades = decades.join('\n');
-            console.log(decades);
-            interaction.reply({content: decades, ephemeral: true});
+
+            // Prepares and sends rosary
+            const rosaryEmbed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('VIRTUAL ROSARY 🙏')
+                .setAuthor({ name: `By ${interaction.user.username}`, iconURL: interaction.user.avatarURL()})
+                .setDescription(decades)
+            interaction.channel.send({ embeds: [rosaryEmbed] });
+
+            return interaction.reply({content: 'Saint Anonymous organized the rosary 🙏', ephemeral: true});
         }
 }
