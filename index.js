7c19a17b92c3614905fe3e363ee7bdf6476cd881
Joshua Cadavez <>
2022-05-12 02:04:08 -0400

    Update sched msg

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const mondayReminder = new cron('0 11 * * 1', async function() {
 
 		const reminderEmbed = new MessageEmbed()
 			.setTitle('Good Morning!')
-			.setDescription('My name is Saint Anonymous. \n\nI share both your prayers and concerns anonymously. \n\nPlease share your prayer requests for rosary tonight. If something or someone concerns you, share it as a concern. \n\nGod bless you and take care!')
+			.setDescription('My name is Saint Anonymous. \n\nI share both your prayers and concerns anonymously. \n\nPlease share your prayer requests for rosary tonight. If something or someone concerns you, share it as a concern and the CORE group will look into it. \n\nGod bless you and take care!')
 			.setColor('#add8e6')
 			.addFields(
 				{
