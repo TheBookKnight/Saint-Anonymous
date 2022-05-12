b464c87c6e23af58bc26ce145b2a764f0f92b5e5
Joshua Cadavez <>
2022-05-11 23:38:41 -0400

    Add concern command

---
+++ commands/concern.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('concern')
+        .setDescription('Shares your concerns anonymously to the CORE group.')
+        .addStringOption(option => option
+            .setName('concern')
+            .setDescription("What are you concerned about?")
+            .setRequired(true)),
+        async execute(interaction) {
+            try {
+                let coreChannel = await interaction.guild.channels.fetch()
+                    .then(channels => {
+                        const targetChannel = channels.find(channel => 
+                            {
+                                return channel.name.toLowerCase().includes("joshua’s-channel") && channel.type == 'GUILD_TEXT';
+                            }) // eventually 'Core Group'
+                        return targetChannel;
+                    })
+                if (coreChannel) {
+                    const concernEmbed = new MessageEmbed()
+                        .setColor('#ff0000')
+                        .setTitle('ANON CONCERN❗')
+                        .setDescription(interaction.options.getString('concern'))
+                    coreChannel.send({ embeds: [concernEmbed]})
+                    return interaction.reply({ content: 'Saint Anonymous shared your concern.', ephemeral: true})
+                } else {
+                    return interaction.reply({ content: 'Saint Anonymous needs a "Core Group" channel to share concerns.', ephemeral: true})
+                }
+            } catch(error) {
+                console.log(error);
+                return interaction.reply({ content: 'Saint Anonymous is currently facing problems... contact your CORE leader', ephemeral: true})
+            }
+        }
+}
