8e4f4f7d730f8fd22c29c75be9e433dac062dacf
Joshua Cadavez <>
2022-05-12 01:42:34 -0400

    Update tutorial

--- commands/active/tutorial.js
+++ commands/active/tutorial.js
@@ -1,9 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const tutorialDMBot = 'Directly message Saint Anonymous your prayer. It will share it to the #prayer-requests channel anonymously. Only works if it has one.'
-const tutorialMusic = 'Slash command "/play-music" followed by the YouTube video url. It will start playing it in the #music channel. Only works if it has one.'
+const tutorialPrayer = 'Directly message Saint Anonymous your prayer. It will share it to the #prayer-requests channel anonymously.'
+const tutorialConcern = 'Slash command `/concern` followed by your concern. It will share it to the Core Group channel anonymously.'
+const tutorialMusic = 'Slash command `/play-music` followed by the YouTube video url. It will start playing it in the #music channel.'
 
-const message = `To use Saint Anonymous...\n\n${tutorialDMBot}\n\n${tutorialMusic}`;
+const message = `To use Saint Anonymous...\n\n${tutorialPrayer}\n\n${tutorialConcern}\n\n${tutorialMusic}`;
 
 module.exports = {
     data: new SlashCommandBuilder()
